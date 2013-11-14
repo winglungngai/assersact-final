a354d0507462d8858bc9572effd940f8d586c032
Joseph Hejderup <j.i.hejderup@student.tudelft.nl>
2013-11-14 14:48:51 +0100

    Changed the taskInfo to String to avoid serialization problesm

--- awseract/awseract-shared/src/main/scala/nl/in4392/models/Task.scala
+++ awseract/awseract-shared/src/main/scala/nl/in4392/models/Task.scala
@@ -3,7 +3,7 @@ package main.scala.nl.in4392.models
 import nl.tudelft.ec2interface.taskmonitor._
 object Task {
 
-  case class Task(taskId: String, job: Any, taskInfo: TaskInfo)
-  case class TaskResult(taskId: String, result: Any, taskInfo: TaskInfo)
+  case class Task(taskId: String, job: Any, taskInfo: String)
+  case class TaskResult(taskId: String, result: Any, taskInfo: String)
 
 }

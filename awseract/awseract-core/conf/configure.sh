bfe0f576dc3b13a495fad7ec68e103027c3335ca
Wing Lung Ngai <w.l.ngai@student.tudelft.nl>
2013-11-15 02:17:49 +0100

    missing applicaiton.conf

--- awseract/awseract-core/conf/configure.sh
+++ awseract/awseract-core/conf/configure.sh
@@ -2,3 +2,5 @@ sed -i 's/DefaultMasterPublicIP/'"$1"'/g' /home/ubuntu/Project/assersact-final/a
 sed -i 's/DefaultPublicIP/'"$2"'/g' /home/ubuntu/Project/assersact-final/awseract/awseract-core/conf/masterInfo
 sed -i 's/DefaultInstanceID/'"$3"'/g' /home/ubuntu/Project/assersact-final/awseract/awseract-core/conf/masterInfo
 sed -i 's/DefaultMasterPublicIP/'"$1"'/g' /home/ubuntu/Project/assersact-final/awseract/awseract-core/src/main/resources/application.conf
+sed -i 's/DefaultPublicIP/'"$2"'/g' /home/ubuntu/Project/assersact-final/awseract/awseract-core/src/main/resources/application.conf
+sed -i 's/DefaultInstanceID/'"$3"'/g' /home/ubuntu/Project/assersact-final/awseract/awseract-core/src/main/resources/application.conf

505917e75251528d0dcf9558ea273cf7b9b2591b
Joseph Hejderup <j.i.hejderup@student.tudelft.nl>
2013-11-15 21:42:27 +0100

    Reactive handling of the jobExecutor

--- awseract/awseract-core/src/main/scala/nl/in4392/worker/WorkerActor.scala
+++ awseract/awseract-core/src/main/scala/nl/in4392/worker/WorkerActor.scala
@@ -10,6 +10,14 @@ import akka.actor.ActorPath
 import java.util.UUID
 import nl.tudelft.ec2interface.taskmonitor.TaskInfo
 import java.sql.Timestamp
+import akka.util.Timeout
+import scala.concurrent.{Await, Future, ExecutionContext}
+import scala.concurrent.duration._
+import akka.pattern.{ ask, pipe }
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits._
+import scala.util.{ Success, Failure }
 import nl.tudelft.ec2interface.instancemanager._
 
 class WorkerActor(workerId: String,masterPath: ActorPath) extends Actor with ActorLogging {
@@ -29,6 +37,31 @@ class WorkerActor(workerId: String,masterPath: ActorPath) extends Actor with Act
        master ! WorkerRegister(workerId)
 
   }
+
+
+  def receive = {
+    case TaskAvailable  =>
+      master ! WorkerRequestTask(workerId)
+    case task: Task =>
+      var tInfo = new TaskInfo().FromJson(task.taskInfo)
+      tInfo.setWorkerId(instanceId)
+      tInfo.setStartTime(new Timestamp(System.currentTimeMillis()))
+      println("Job info: id {}, job {}",task.taskId, task.taskInfo.toString)
+      implicit val timeout = Timeout(10 seconds)
+      val resultFuture = jobExecutor ? new Task(task.taskId,task.job,new TaskInfo().ToJson(tInfo))
+      resultFuture onComplete {
+        case Success(TaskResult(taskId,result,taskInfo)) =>
+          println("Task Finished. Result {}.", result)
+          var tInfo = new TaskInfo().FromJson(taskInfo)
+          tInfo.setFinishTime(new Timestamp(System.currentTimeMillis()))
+          master ! TaskCompleted(workerId,taskId,result,new TaskInfo().ToJson(tInfo))
+          master ! WorkerRequestTask(workerId)
+        case Failure(t) =>
+          t.printStackTrace()
+      }
+  }
+
+ /*
 
   def stateIdle: Receive = {
 
@@ -62,5 +95,6 @@ class WorkerActor(workerId: String,masterPath: ActorPath) extends Actor with Act
 
 
   def receive = stateIdle
+  */
 
 }
